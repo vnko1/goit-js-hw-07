b880cca517ea7cede45150b4d632fb89235f6a48
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-17 13:30:02 +0200

    s

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onModalOpenClick);
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+function onModalOpenClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  createLightBox(e.target.dataset.source);
+  document.addEventListener("keydown", onCloseLightBoxEsc);
+  document.addEventListener("click", onCloseLightBoxClick);
+}
+
+// function onCloseLightBox(e) {
+//   document;
+// }
+
+function createLightBox(source) {
+  basicLightbox
+    .create(
+      `
+	<img src="${source}" width="800" height="600">
+`,
+      { closable: false }
+    )
+    .show();
+}
