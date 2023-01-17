312767771a69cb60a35bca672b967d71d8242617
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-17 16:17:01 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onLightBoxOpenClick);
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>`
+    )
+    .join("");
+}
+
+function onLightBoxOpenClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
